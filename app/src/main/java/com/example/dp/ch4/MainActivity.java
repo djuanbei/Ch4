2fda8a9472585472fa6a1430836b4058e416489f
Liyun Dai <dlyun2009@gmail.com>
2018-10-12 10:43:01 +0800

    LauncherActivity 需要显示界面上有List

--- app/src/main/java/com/example/dp/ch4/MainActivity.java
+++ app/src/main/java/com/example/dp/ch4/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.dp.ch4;
 
+import android.app.LauncherActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends LauncherActivity {
+
+    String[] names = {"设置程序参数", "查看星际兵种"};
+
+    Class<?>[] clazzs = {PreferenceAcitivityTest.class, ExpandableListActivityTest.class};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //  setContentView(R.layout.main); //界面中必须有List
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public Intent intentForPosition(int position) {
+        return new Intent(MainActivity.this, clazzs[position]);
     }
 }
