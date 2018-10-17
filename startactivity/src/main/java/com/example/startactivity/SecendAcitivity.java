ad89c2bcedbff64e084f7fc98ab692908cfeb42a
Liyun Dai <dlyun2009@gmail.com>
2018-10-17 16:44:36 +0800

    LauncherActivity 需要显示界面上有List

--- startactivity/src/main/java/com/example/startactivity/SecendAcitivity.java
+++ startactivity/src/main/java/com/example/startactivity/SecendAcitivity.java
@@ -1,27 +1,39 @@
 package com.example.startactivity;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class SecendAcitivity extends AppCompatActivity {
 
+
+    Handler h=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            // ...
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secend_acitivity);
+        h.postDelayed();
     }
 
 
     public void start(View source){
-
         Intent intent=new Intent(SecendAcitivity.this, MainActivity.class);
         startActivity(intent);
     }
 
     public void close(View source){
-        Intent intent=new Intent(SecendAcitivity.this, MainActivity.class);
+      //  Intent intent=new Intent(SecendAcitivity.this, MainActivity.class);
+        Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.baidu.com"));
         startActivity(intent);
         finish();
     }
